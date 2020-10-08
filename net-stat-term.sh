dae8ce6e530dff0181e5bb7c33b17d2b3234f4c3
jcabaniss <56398381+jcabaniss@users.noreply.github.com>
2020-10-08 09:53:49 -0400

    Update net-stat-term.sh
    Grep -v grep added to avoid error messages that annoy people.

--- net-stat-term.sh
+++ net-stat-term.sh
@@ -1,4 +1,4 @@
 #This must have a name that does not contain "netstat"
-ps aux | grep netstat | awk '{print "kill -9 "$2}' > cmd.sh
+ps aux | grep netstat | grep -v grep | awk '{print "kill -9 "$2}' > cmd.sh
 chmod +x cmd.sh
 ./cmd.sh
