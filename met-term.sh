6a4076242e9894e8407aa3bf436b54b4661df9cc
jcabaniss <56398381+jcabaniss@users.noreply.github.com>
2020-10-12 12:37:52 -0400

    Update met-term.sh

--- met-term.sh
+++ met-term.sh
@@ -1,4 +1,4 @@
-#This must have a name that does not contain "netstat"
+#This must have a name that does not contain "metrics.sh"
 ps aux | grep metrics.sh | grep -v grep | awk '{print "kill -9 "$2}' > cmd.sh
 chmod +x cmd.sh
 ./cmd.sh
