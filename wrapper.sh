8d0fe03e49014f9e850fb2fdf9c1912064afd621
jcabaniss <56398381+jcabaniss@users.noreply.github.com>
2020-10-22 14:02:15 -0400

    Update wrapper.sh
    Fixed type

--- wrapper.sh
+++ wrapper.sh
@@ -7,7 +7,7 @@ sysctl -n hw.physicalcpu
 echo "Reported CPU Frequency (Ignores Turboboosting etc)"
 sysctl -n hw.cpufrequency
 echo "RAM"
-systcl -n hw.memsize
+sysctl -n hw.memsize
 #echo "Kernal Version"
 #Xcode specific info, may not apply to all builds or use cases
 #xcodebuild -version | awk '{if ($1~/Xcode/){print "Xcode";print $2} if ($1~/Build/){print "Build Version"; print $3}}'
