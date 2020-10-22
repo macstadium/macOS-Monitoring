c8d9f19e0a43fe1eb6e8e05296a8a94fbf7d8866
jcabaniss <56398381+jcabaniss@users.noreply.github.com>
2020-10-22 13:57:20 -0400

    Update wrapper.sh
    Commented out xcode mandatory items so script will run as intended on any system

--- wrapper.sh
+++ wrapper.sh
@@ -8,9 +8,9 @@ echo "Reported CPU Frequency (Ignores Turboboosting etc)"
 sysctl -n hw.cpufrequency
 echo "RAM"
 systcl -n hw.memsize
-echo "Kernal Version"
+#echo "Kernal Version"
 #Xcode specific info, may not apply to all builds or use cases
-xcodebuild -version | awk '{if ($1~/Xcode/){print "Xcode";print $2} if ($1~/Build/){print "Build Version"; print $3}}'
+#xcodebuild -version | awk '{if ($1~/Xcode/){print "Xcode";print $2} if ($1~/Build/){print "Build Version"; print $3}}'
 ./metrics.sh > build.log
 sleep 10
 #Run commands for your build here or call another file here
