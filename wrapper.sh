802d8cc66d306c63b1c906fedff31280eaf5e77a
jcabaniss <56398381+jcabaniss@users.noreply.github.com>
2020-10-23 15:35:43 -0400

    Add files via upload
    Wrapper.sh now defaults to build.log for simplicity

--- wrapper.sh
+++ wrapper.sh
@@ -1,19 +1,16 @@
-echo "Time & Date"
-date
-echo "Logical Cores"
-sysctl -n hw.ncpu
-echo "Physical CPU (same for VMs)"
-sysctl -n hw.physicalcpu
-echo "Reported CPU Frequency (Ignores Turboboosting etc)"
-sysctl -n hw.cpufrequency
-echo "RAM"
-systcl -n hw.memsize
-echo "Kernal Version"
+echo "Time & Date" > build.log
+date >> build.log
+echo "Logical Cores" >> build.log
+sysctl -n hw.ncpu >> build.log
+echo "Physical CPU (same for VMs)" >> build.log
+sysctl -n hw.physicalcpu >> build.log
+echo "Reported CPU Frequency (Ignores Turboboosting etc)" >> build.log
+sysctl -n hw.cpufrequency >> build.log
+echo "RAM" >> build.log
+sysctl -n hw.memsize >> build.log
+#echo "Kernal Version"
 #Xcode specific info, may not apply to all builds or use cases
-xcodebuild -version | awk '{if ($1~/Xcode/){print "Xcode";print $2} if ($1~/Build/){print "Build Version"; print $3}}'
-./metrics.sh > build.log
-sleep 10
+#xcodebuild -version | awk '{if ($1~/Xcode/){print "Xcode";print $2} if ($1~/Build/){print "Build Version"; print $3}}'
+./metrics.sh >> build.log
 #Run commands for your build here or call another file here
-sleep 10
-./net-stat-term.sh
-./met-term.sh
+#./term.sh
